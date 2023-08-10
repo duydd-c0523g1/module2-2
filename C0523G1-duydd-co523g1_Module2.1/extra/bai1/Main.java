c754c82de97c5d27959e99dc2583d5eefd786df5
duydd-c0523g1 <daoducduy.work@gmail.com>
2023-08-10 13:31:36 +0700

    1st commit

--- C0523G1-duydd-co523g1_Module2.1/extra/bai1/Main.java
+++ C0523G1-duydd-co523g1_Module2.1/extra/bai1/Main.java
@@ -2,15 +2,22 @@ package extra.bai1;
 
 import src.bai7_abstraction.exercise.resizable.Circle;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("First number = ");
-        int num1 = Integer.parseInt(scanner.nextLine());
-        System.out.print("Second number = ");
-        int num2 = Integer.parseInt(scanner.nextLine());
-        System.out.println(Calculator.calculate(num1, num2));
+        do {
+            try {
+                System.out.print("First number = ");
+                int num1 = Integer.parseInt(scanner.nextLine());
+                System.out.print("Second number = ");
+                int num2 = Integer.parseInt(scanner.nextLine());
+                System.out.println(Calculator.calculate(num1, num2));
+            } catch (InputMismatchException | NumberFormatException e) {
+                System.out.println("Lỗi");
+            }
+        } while (true);
     }
 }
