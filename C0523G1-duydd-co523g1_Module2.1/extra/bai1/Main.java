93c3b4483afc0874c3e2400fbc1a245f1484df46
duydd-c0523g1 <daoducduy.work@gmail.com>
2023-08-10 13:33:26 +0700

    1st commit

--- C0523G1-duydd-co523g1_Module2.1/extra/bai1/Main.java
+++ C0523G1-duydd-co523g1_Module2.1/extra/bai1/Main.java
@@ -8,16 +8,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        do {
-            try {
-                System.out.print("First number = ");
-                int num1 = Integer.parseInt(scanner.nextLine());
-                System.out.print("Second number = ");
-                int num2 = Integer.parseInt(scanner.nextLine());
-                System.out.println(Calculator.calculate(num1, num2));
-            } catch (InputMismatchException | NumberFormatException e) {
-                System.out.println("Lỗi");
-            }
+        do try {
+            System.out.print("First number = ");
+            int num1 = Integer.parseInt(scanner.nextLine());
+            System.out.print("Second number = ");
+            int num2 = Integer.parseInt(scanner.nextLine());
+            System.out.println(Calculator.calculate(num1, num2));
+        } catch (InputMismatchException | NumberFormatException e) {
+            System.out.println("Lỗi");
         } while (true);
     }
 }
