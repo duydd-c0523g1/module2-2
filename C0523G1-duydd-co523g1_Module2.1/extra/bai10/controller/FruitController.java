136d0e335e5660649b89a5f684ce38d4f5315896
duydd-c0523g1 <daoducduy.work@gmail.com>
2023-08-15 11:34:46 +0700

    1st commit

--- C0523G1-duydd-co523g1_Module2.1/extra/bai10/controller/FruitController.java
+++ C0523G1-duydd-co523g1_Module2.1/extra/bai10/controller/FruitController.java
@@ -7,31 +7,34 @@ import java.util.Scanner;
 
 public class FruitController {
     private static IFruitService fruitService = new FruitServiceImpl();
+
     public static void showMenu() {
         Scanner scanner = new Scanner(System.in);
-        do try {
-            int option;
-            System.out.println("=====FRUITS MENU=====");
-            System.out.println("1. Add new fruit");
-            System.out.println("2. Display fruit list");
-            System.out.println("3. Exit");
-            System.out.print("Insert your option: ");
-            option = Integer.parseInt(scanner.nextLine());
-            System.out.println("-----------------------------------");
-            switch (option) {
-                case 1:
-                    fruitService.addNew();
-                    break;
-                case 2:
-                    fruitService.displayAll();
-                    break;
-                case 3:
-                    System.exit(0);
-                default:
-                    System.out.println("Invalid option, please try again.");
+        do {
+            try {
+                int option;
+                System.out.println("=====FRUITS MENU=====");
+                System.out.println("1. Add new fruit");
+                System.out.println("2. Display fruit list");
+                System.out.println("3. Exit");
+                System.out.print("Insert your option: ");
+                option = Integer.parseInt(scanner.nextLine());
+                System.out.println("-----------------------------------");
+                switch (option) {
+                    case 1:
+                        fruitService.addNew();
+                        break;
+                    case 2:
+                        fruitService.displayAll();
+                        break;
+                    case 3:
+                        System.exit(0);
+                    default:
+                        System.out.println("Invalid option, please try again.");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Please input a number!!");
             }
-        } catch (NumberFormatException e) {
-            System.out.println("Please input a number!!");
-        } while (true);
+        } while (true) ;
     }
 }
