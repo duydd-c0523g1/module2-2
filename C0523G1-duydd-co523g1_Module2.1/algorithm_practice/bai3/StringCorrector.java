eafb56b0730317dc7ae58f914e4b06efda4814fb
duydd-c0523g1 <daoducduy.work@gmail.com>
2023-08-16 18:11:29 +0700

    1st commit

---
+++ C0523G1-duydd-co523g1_Module2.1/algorithm_practice/bai3/StringCorrector.java
@@ -0,0 +1,22 @@
+package algorithm_practice.bai3;
+
+public class StringCorrector {
+    public static void main(String[] args) {
+        String string = "chuong TrinhXuLiXauKyTu";
+        String result = correctString(string);
+        System.out.println(result);
+    }
+    public static String correctString(String input) {
+        StringBuilder output = new StringBuilder();
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if (i > 0 && Character.isUpperCase(c)) {
+                output.append(' ');
+                output.append(Character.toLowerCase(c));
+            } else {
+                output.append(Character.toLowerCase(c));
+            }
+        }
+        return output.toString();
+    }
+}
