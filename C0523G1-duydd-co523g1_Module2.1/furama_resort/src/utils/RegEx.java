2aa58710bad020a99c68904e66891577dc91c9f9
duydd-c0523g1 <daoducduy.work@gmail.com>
2023-08-31 11:17:59 +0700

    commit

--- C0523G1-duydd-co523g1_Module2.1/furama_resort/src/utils/RegEx.java
+++ C0523G1-duydd-co523g1_Module2.1/furama_resort/src/utils/RegEx.java
@@ -29,9 +29,8 @@ public class RegEx {
     }
 
     public static boolean regexIdentNumber(String id) {
-        Pattern pattern1 = Pattern.compile("^\\d{9}$");
-        Pattern pattern2 = Pattern.compile("^\\d{12}$");
-        return pattern1.matcher(id).matches() || pattern2.matcher(id).matches();
+        Pattern pattern = Pattern.compile("^\\d{9}$|^\\d{12}$");
+        return pattern.matcher(id).matches();
     }
 
     public static boolean regexPhoneNumber(String phoneNumber) {
